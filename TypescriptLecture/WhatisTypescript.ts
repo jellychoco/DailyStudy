879aabb1d77d8b68dc5b34b738d5d655b5fa7bbe
jellychoco <ma88082@gmail.com>
2021-02-22 23:59:44 +0900

    타입스크립트 기본개념

---
+++ TypescriptLecture/WhatisTypescript.ts
@@ -0,0 +1,37 @@
+
+
+// 1. 타입스크립트란?
+
+// 만든곳 : 마이크로소프트
+// 장점 : 자바스크립트의 특징인 동적타입(Dynamically typed)은 프로그램이 동작할 때 타입이 결정되므로,
+// 런타임 환경에서 에러가 날수 있는 반면,
+//          타입스크립트는 정적타입(Statically Typed)이라 컴파일 시간 때 에러를 잡을수 있다.
+
+
+
+// 2. 타입스크립트를 배워야 하는 이유
+
+//  1) 첫번째 : 
+//     => 에러를 캐치 하는 순간이 더 빠르다 
+
+const diffrenceTypeLangauge = {
+    dynamicallyTypes : ['python','ruby','Lua','php','javascript'],
+    StaticallyTypes: ['typescript','Java','kotlin','scala','swift','go','C','C++']
+}
+
+//코드 실행 프로세스
+
+// 우리가 친 코드가 -> 컴파일타임(컴파일러)를 거쳐 변환이 완료 되면 -> 런타임에서 실행된다
+//              -> 이 구간에서 타 입이 결정되는 것이 staticallyTypes -> 여기는v   dynamicallyTypes
+
+
+
+
+//  2) OOP
+//    OOP로 코딩하면 =>  객체위주로 모듈성 있는 코드 = modulartiy, 그렇기에 재사용성 UP = reusability
+//               객체단위로 확장가능 = extensible, 이것들은 근본으로해서 기존 코드의 문제해결 및 신기능 추가가능 = maintainability
+
+
+
+
+
